dde55c77ddbd2921e7082c1abea0dd88cee711d9
Zoumana Keita <37105724+keitazoumana@users.noreply.github.com>
2022-08-29 08:10:31 -0500

    Update get_data.py
    Updating the file to configure DVC

--- get_data.py
+++ get_data.py
@@ -1,4 +1,24 @@
+# Set Authentication
+from yaml import safe_load 
 import os
 
+# Get The Credentials 
+credentials = safe_load(open("config.yaml"))["credentials"]
+
+# DVC Credentials
+dvc_config = credentials["dvc_config"]
+DVC_REMOTE_URL = dvc_config["DAGSHUB_REMOTE_URL"]
+USERNAME = dvc_config["USERNAME"]
+PASSWORD = dvc_config["PASSWORD"]
+
+# Get Raw Data path for pulling
+meta_data = credentials["metadata_path"]
+RAW_DATA_PATH = meta_data["RAW_DATA_PATH"]
+
+os.system("dvc remote add origin {}".format(DVC_REMOTE_URL))
+os.system("dvc remote modify origin --local auth basic")
+os.system("dvc remote modify origin --local user {}".format(USERNAME))
+os.system("dvc remote modify origin --local password {}".format(PASSWORD))
+
 # Get data from DVC
-os.system("dvc pull -r origin data/raw")
+os.system("dvc pull -r origin {}".format(RAW_DATA_PATH))
